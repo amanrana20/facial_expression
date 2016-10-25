9ce02cd72f1fa206496c064dcfcd7124e43222db
Aman Rana <amanrana20@gmail.com>
2016-10-24 20:04:47 -0400

    adding updated code

--- cnn.py
+++ cnn.py
@@ -1,4 +1,6 @@
 import numpy as np
+from keras import backend as K
+K.set_image_dim_ordering('th')
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -7,21 +9,25 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 import pandas as pd
-import cv2
 
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
 
-PATH_MAIN = '/Users/amanrana/Documents/Deep Learning Project/'
-EXT_DATASET = 'FacialExpresionDataset/fer2013.csv'
-dataset = pd.read_csv(PATH_MAIN+EXT_DATASET)
+URL_DATASET = 'fer2013.csv'
+dataset = pd.read_csv(URL_DATASET)
 
 X_train = []
 
 X_raw = dataset['pixels']
 for row in X_raw:
     X_train.append(row.split(' '))
+
+
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#print(pd.DataFrame(X_train).skew())
+
 
 Y_train = np_utils.to_categorical(np.array(dataset['emotion']))
 print len(Y_train)
@@ -32,26 +38,23 @@ num_classes = 7
 # Creating the model
 model = Sequential()
 model.add(Convolution2D(32, 5, 5, border_mode='valid', input_shape=(1, 48, 48), activation='relu'))
+model.add(Convolution2D(32, 5, 5, border_mode='valid', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
+model.add(Convolution2D(64, 5, 5, border_mode='valid', activation='relu'))
 model.add(Convolution2D(64, 5, 5, border_mode='valid', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
-model.add(Convolution2D(128, 5, 5, border_mode='valid', activation='relu'))
+model.add(Convolution2D(128, 3, 3, border_mode='valid', activation='relu'))
+model.add(Convolution2D(128, 3, 3, border_mode='valid', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
-model.add(Convolution2D(256, 5, 5, border_mode='valid', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(num_classes, activation='sigmoid'))
+print model.summary()
 
 # Compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train[100:], Y_train[100:], validation_split=0.1, nb_epoch=10, batch_size=8, verbose=2)
+model.fit(X_train[100:], Y_train[100:], validation_split=0.1, nb_epoch=10, batch_size=100, verbose=2)
 
 # Evaluating the model
 scores = model.evaluate(X_train[:100], Y_train[:100], verbose=0)
